649c471caaf2a458b95caf1188c245a1b8247485
keemzeehae <keemzeehae@naver.com>
2022-01-04 12:19:29 +0900

    javaStudyTest

---
+++ src/com/jihye/s1/method/JavaStudyTest.java
@@ -0,0 +1,11 @@
+package com.jihye.s1.method;
+
+public class JavaStudyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java Study Test");
+
+	}
+
+}
